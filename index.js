2c630dd2be7969acf801aa92386894a73feb868d
Cesar <jotamcgdeveloper@gmail.com>
2022-03-21 20:02:28 +0100

    Git inicial del sevidoFin sección 3

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+// APP de Express
+const app = express();
+
+// NODE SERVER
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+
+require('./sockets/socket');
+
+
+
+
+
+//Path publico
+const publicPath = path.resolve( __dirname, 'public');
+
+app.use(express.static(publicPath));
+
+
+
+server.listen(process.env.PORT, (err) => {
+
+    if(err) throw new Error(err);
+
+    console.log('Servidor corriendo en puerto', 3000);
+})
